2722547d2b5a107f29d911c19712a41f68f0f00d
TheAllegedGenius <91752579+TheAllegedGenius@users.noreply.github.com>
2024-02-09 03:21:50 -0500

    status.sh updated

--- sway/status.sh
+++ sway/status.sh
@@ -1,31 +1,47 @@
+#! /usr/bin/bash
 date_formatted=$(date "+%a %b %d %H:%M")
+method2 () {
+    battery0="/sys/class/power_supply/BAT0"
+    battery1="/sys/class/power_supply/BAT1"
 
-start_time=$(date +%s)
-start_cpu=$(ps -p $$ -o %cpu | awk 'NR==2')
+    battery0_capacity=$(cat $battery0/energy_full)
+    battery0_remaining=$(cat $battery0/energy_now)
+    if [ -d $battery1 ]; then
+        battery1_capacity=$(cat $battery1/energy_full)
+        battery1_remaining=$(cat $battery1/energy_now)
+        total_capacity=$(echo "$battery0_capacity + $battery1_capacity" | bc)
+        total_remaining=$(echo "$battery0_remaining + $battery1_remaining" | bc)
+    else
+        total_capacity=$battery0_capacity
+        total_remaining=$battery0_remaining
+    fi
 
-battery0=$(upower --show-info /org/freedesktop/UPower/devices/battery_BAT0)
-battery1=$(upower --show-info /org/freedesktop/UPower/devices/battery_BAT1)
+    battery_percent_remaining=$(echo "scale=4; ($total_remaining / $total_capacity) * 100" | bc)
 
-battery0_capacity=$(echo "$battery0" | awk 'NR==16 {print $2}') 
-battery1_capacity=$(echo "$battery1" | awk 'NR==16 {print $2}') 
+    echo "${battery_percent_remaining%00}%"
+}
 
-#battery0_capacity=$(echo "$battery0" | grep "energy-full:" | awk '{print $2}') 
-#battery1_capacity=$(echo "$battery1" | grep "energy-full:" | awk '{print $2}') 
-total_capacity=$((echo "$battery0_capacity + $battery1_capacity" | bc))
+method3 () {
+    battery0="/sys/class/power_supply/BAT0"
+    battery1="/sys/class/power_supply/BAT1"
 
-battery0_remaining=$(echo "$battery0" | awk 'NR==14 {print $2}') 
-battery1_remaining=$(echo "$battery1" | awk 'NR==14 {print $2}') 
+    battery0_capacity=$(cat $battery0/energy_full)
+    battery0_remaining=$(cat $battery0/energy_now)
+    if [ -d $battery1 ]; then
+        battery1_capacity=$(cat $battery1/energy_full)
+        battery1_remaining=$(cat $battery1/energy_now)
+        total_capacity=$(echo "$battery0_capacity + $battery1_capacity" | bc)
+        total_remaining=$(echo "$battery0_remaining + $battery1_remaining" | bc)
+        battery_percent_remaining=$(echo "scale=4; ($total_remaining / $total_capacity) * 100" | bc)
+    else
+        total_capacity=$battery0_capacity
+        total_remaining=$battery0_remaining
+        battery_percent_remaining=$(echo "scale=4; ($battery0_remaining / $battery0_capacity) * 100" | bc)
+    fi
 
-#battery0_capacity=$(echo "$battery0" | grep "energy:" | awk '{print $2}') 
-#battery1_capacity=$(echo "$battery1" | grep "energy:" | awk '{print $2}') 
-total_remaining=$((echo "$battery0_remaining + $battery1_remaining" | bc))
+    echo "${battery_percent_remaining%00}%"
+}
 
-battery_percent_remaining=$(echo "scale=2; $total_remaining / $total_capacity * 100" | bc)
-
-end_cpu=$(ps -p $$ -o %cpu | awk 'NR==2')
-end_time=$(date +%s)
-
-time=$((end-time - start-time))
-cpu=$((end_cpu - start_cpu))
-echo "Elapsed time: $time seconds"
-echo "CPU usage: $cpu%"
+times5 "method1"
+times5 "method2"
+times5 "method3"
