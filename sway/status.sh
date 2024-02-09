9f3f86328eabcca848569e53df7fea5d8b42cc4c
TheAllegedGenius <91752579+TheAllegedGenius@users.noreply.github.com>
2024-02-09 01:21:26 -0500

    status.sh worked on

--- sway/status.sh
+++ sway/status.sh
@@ -1,11 +1,31 @@
 date_formatted=$(date "+%a %b %d %H:%M")
 
-battery0_capacity=$(upower --show-info $(upower --enumerate) | grep "BAT0" | grep "energy-full:" | sed 's/%//')
-battery1_capacity=$(upower --show-info $(upower --enumerate) | grep "BAT0" | grep "energy-full:" | sed 's/%//')
-total_capacity=$(($battery0_capacity + $battery1_capacity))
+start_time=$(date +%s)
+start_cpu=$(ps -p $$ -o %cpu | awk 'NR==2')
 
-battery0_remaining=$(upower --show-info $(upower --enumerate) | grep "BAT0" | grep "energy:" | sed 's/%//')
-battery1_remaining=$(upower --show-info $(upower --enumerate) | grep "BAT0" | grep "energy:" | sed 's/%//')
-total_remaining=$(($battery0_remaining + $battery1_remaining))
+battery0=$(upower --show-info /org/freedesktop/UPower/devices/battery_BAT0)
+battery1=$(upower --show-info /org/freedesktop/UPower/devices/battery_BAT1)
 
-battery_percent_remaining=$(($total_remaining / $total_capacity))
+battery0_capacity=$(echo "$battery0" | awk 'NR==16 {print $2}') 
+battery1_capacity=$(echo "$battery1" | awk 'NR==16 {print $2}') 
+
+#battery0_capacity=$(echo "$battery0" | grep "energy-full:" | awk '{print $2}') 
+#battery1_capacity=$(echo "$battery1" | grep "energy-full:" | awk '{print $2}') 
+total_capacity=$((echo "$battery0_capacity + $battery1_capacity" | bc))
+
+battery0_remaining=$(echo "$battery0" | awk 'NR==14 {print $2}') 
+battery1_remaining=$(echo "$battery1" | awk 'NR==14 {print $2}') 
+
+#battery0_capacity=$(echo "$battery0" | grep "energy:" | awk '{print $2}') 
+#battery1_capacity=$(echo "$battery1" | grep "energy:" | awk '{print $2}') 
+total_remaining=$((echo "$battery0_remaining + $battery1_remaining" | bc))
+
+battery_percent_remaining=$(echo "scale=2; $total_remaining / $total_capacity * 100" | bc)
+
+end_cpu=$(ps -p $$ -o %cpu | awk 'NR==2')
+end_time=$(date +%s)
+
+time=$((end-time - start-time))
+cpu=$((end_cpu - start_cpu))
+echo "Elapsed time: $time seconds"
+echo "CPU usage: $cpu%"
