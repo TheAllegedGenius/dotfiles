0d7184106dad675a400edbdbb7061c5ac50ae581
TheAllegedGenius <91752579+TheAllegedGenius@users.noreply.github.com>
2024-02-15 12:26:36 -0500

    Added emojis to foot, sway, and swaybar

--- sway/status.sh
+++ sway/status.sh
@@ -13,21 +13,48 @@ function battery_status {
         local total_remaining=$(echo "$battery0_remaining + $battery1_remaining" | bc)
         local battery0_status=$(cat $battery0/status)
         local battery1_status=$(cat $battery1/status)
-        if [[ $battery0_status == "Full" && $battery1_status == "Full" ]]; then
-            battery_status="Full"
-        elif [[ $battery0_status == "Charging" || $battery1_status == "Charging" ]]; then
-            battery_status="Charging"
+        if [[ $battery0_status == "Full" ]]; then
+            battery0_status="🔋"
+        elif [[ $battery0_status == "Charging" ]]; then
+            battery0_status="🔌"
         else
-            battery_status="Int: $battery0_status Ext: $battery1_status"
+            battery0_status="🪫"
         fi
+        if [[ $battery1_status == "Full" ]]; then
+            battery1_status="🔋"
+        elif [[ $battery1_status == "Charging" ]]; then
+            battery1_status="🔌"
+        else
+            battery1_status="🪫"
+        fi
+        battery_status="I$battery0_status E$battery1_status"
     else
         local total_capacity=$battery0_capacity
         local total_remaining=$battery0_remaining
-        battery_status=$(cat $battery0/status)
+        battery0_status=$(cat $battery0/status)
+        if [[ $battery0_status == "Full" ]]; then
+            battery_status="🔋"
+        elif [[ $battery0_status == "Charging" ]]; then
+            battery_status="🔌"
+        else
+            battery_status="🪫"
+        fi
     fi
 
     local battery_percent_remaining=$(echo "scale=4; ($total_remaining / $total_capacity) * 100" | bc)
+    if [ $battery_percent_remaining < 80.00 ]; then
+        battery_status="⚡️"
+    fi
     battery_percentage=$(echo "${battery_percent_remaining%00}%")
 }
+current_volume=$(pamixer --get-volume-human)
+volume_is_muted=$(pamixer --get-mute)
+if [ $current_volume = "true" ]
+then
+        audio_active="🔇"
+    else
+        audio_active="🔊"
+fi
+
 battery_status
-echo "$battery_percentage ($battery_status) $date_formatted"
+echo "$audio_active $current_volume | $battery_percentage $battery_status | 🕘 $date_formatted"
