0ab92ac93f1ce17e4d32b830908f7a5513c78029
TheAllegedGenius <91752579+TheAllegedGenius@users.noreply.github.com>
2024-02-20 22:13:34 -0500

    Modified nvim remaps

--- nvim/lua/theallegedgenius/remap.lua
+++ nvim/lua/theallegedgenius/remap.lua
@@ -10,11 +10,10 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
-vim.keymap.set("x", "<leader>p", "\"_dP")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
-vim.keymap.set("n", "<CR>", "o<Esc>")
-vim.keymap.set("n", "<S-CR>", "O<Esc>")
+vim.keymap.set("n", "<CR>", "mzo<Esc>`z")
+vim.keymap.set("n", "<S-CR>", "mzO<Esc>`z")
 
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
