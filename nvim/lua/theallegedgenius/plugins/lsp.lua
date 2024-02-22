bd2bcec2e078fb34ba09ce48d1367364e6d06903
TheAllegedGenius <91752579+TheAllegedGenius@users.noreply.github.com>
2024-02-22 10:02:17 -0500

    Fixed nvim LSP completion

--- nvim/lua/theallegedgenius/plugins/lsp.lua
+++ nvim/lua/theallegedgenius/plugins/lsp.lua
@@ -13,6 +13,14 @@ return {
     },
 
     config = function()
+        local cmp = require('cmp')
+        local cmp_lsp = require("cmp_nvim_lsp")
+        local capabilities = vim.tbl_deep_extend(
+            "force",
+            {},
+            vim.lsp.protocol.make_client_capabilities(),
+            cmp_lsp.default_capabilities())
+
         require("mason").setup()
         require("mason-lspconfig").setup({
             ensure_installed = {
@@ -24,12 +32,15 @@ return {
             handlers = {
                 -- default handler (optional)
                 function (server_name)
-                    require("lspconfig")[server_name].setup {}
+                    require("lspconfig")[server_name].setup {
+                        capabilities = capabilities,
+                    }
                 end,
                 -- Next, you can provide targeted overrides for specific servers.
                 ["lua_ls"] = function ()
                     local lspconfig = require("lspconfig")
                     lspconfig.lua_ls.setup {
+                        capabilities = capabilities,
                         settings = {
                             Lua = {
                                 diagnostics = {
@@ -41,7 +52,6 @@ return {
                 end,
             }
         })
-        local cmp = require("cmp")
         local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
         cmp.setup({
@@ -50,10 +60,6 @@ return {
                 expand = function(args)
                     require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
                 end,
-            },
-            window = {
-                -- completion = cmp.config.window.bordered(),
-                -- documentation = cmp.config.window.bordered(),
             },
             mapping = cmp.mapping.preset.insert({
                 ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
