1d16e446fc8627e58c3fee745b16cd40a0966912
TheAllegedGenius <91752579+TheAllegedGenius@users.noreply.github.com>
2026-09-02 13:05:26 -0400

    keymaps.lua: add comments explaining each keymap

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,16 +1,28 @@
+-- Set leader to <Space>
 vim.g.mapleader = " "
+
+-- Open Netrw (file browser)
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Move selected lines up/down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- Join lines with cursor preserved
 vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
+
+-- Move up/down with cursor centered
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+
+--Center on next/previous search result
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
+-- Replace word under cursor
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
-vim.keymap.set("n", "<CR>", "mzo<Esc>`z")
+-- Create new line above/below and stay in normal mode
 vim.keymap.set("n", "<S-CR>", "mzO<Esc>`z")
+vim.keymap.set("n", "<CR>", "mzo<Esc>`z")
+
